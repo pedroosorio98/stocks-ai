85c5b4d4efb64013a2c0c6435e3cc470eb102da5
pedroosorio98 <59745060+pedroosorio98@users.noreply.github.com>
2026-02-01 20:08:13 -0300

    changes

--- app.py
+++ app.py
@@ -271,4 +271,5 @@ def create_chart():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port, debug=False)
